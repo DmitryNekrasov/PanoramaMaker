be28b176fd2142eadc18677b68855e2bbc386389
Dmitry Nekrasov <scannorone@gmail.com>
2016-11-11 20:24:57 +0700

    Initial Commit

---
+++ PanoramaMaker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PanoramaMaker
+//
+//  Created by Dmitry Nekrasov on 11.11.16.
+//  Copyright © 2016 Dmitry Nekrasov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
