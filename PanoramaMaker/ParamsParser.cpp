0a6d18b86b081fbe9f8bfc35bd4dbfa5cf484855
Dmitry Nekrasov <scannorone@gmail.com>
2016-11-11 20:37:36 +0700

    Добавил класс ParamsParser

--- PanoramaMaker/ParamsParser.cpp
+++ PanoramaMaker/ParamsParser.cpp
@@ -1,17 +1,13 @@
 //
-//  main.cpp
+//  ParamsParser.cpp
 //  PanoramaMaker
 //
 //  Created by Dmitry Nekrasov on 11.11.16.
 //  Copyright © 2016 Dmitry Nekrasov. All rights reserved.
 //
 
-#include <iostream>
-
 #include "ParamsParser.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+ParamsParser::ParamsParser(int argc, char** argv) {
+    
 }
---
+++ PanoramaMaker/ParamsParser.hpp
@@ -0,0 +1,17 @@
+//
+//  ParamsParser.hpp
+//  PanoramaMaker
+//
+//  Created by Dmitry Nekrasov on 11.11.16.
+//  Copyright © 2016 Dmitry Nekrasov. All rights reserved.
+//
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+class ParamsParser {
+public:
+    ParamsParser(int argc, char** argv);
+};
